45b3d58ed75cb30706f1abe4c6b2fa20b7a6beaf
Alexandre Cros <alexcros77700@gmail.com>
2023-01-26 12:40:56 +0100

    Test 01 to Test 09 passed

--- src/PlushStore.hpp
+++ src/PlushStore.hpp
@@ -1,1 +1,66 @@
-#pragma once
+#include <string>
+
+class PlushStore
+{
+public:
+    PlushStore(const std::string& name)
+        : _name { name }
+    {}
+
+    std::string get_name() const
+    {
+        return _name;
+    }
+
+    int get_wealth_amount() const
+    {
+        return _wealth;
+    }
+
+    int get_stock_size() const
+    {
+        return _stock;
+    }
+
+    int get_debt_amount() const
+    {
+        return _debt;
+    }
+
+    int get_experience() const
+    {
+        return _experience;
+    }
+
+    void loan(int money)
+    {
+        _wealth += money;
+        _debt += money + (money * 10) / 100;
+    }
+
+    int make_plush(int cost)
+    {
+        if (_wealth != 0)
+        {
+            _stock += 1;
+            _experience += 1;
+        }
+        if (_wealth - cost < 0)
+        {
+            _wealth = 0;
+            return cost - _wealth + std::max(_experience, _experience * cost / 100);
+        }
+        else
+        {
+            _wealth -= cost;
+            return cost + std::max(_experience, _experience * cost / 100);
+        }
+    }
+
+private:
+    std::string _name;
+    int _wealth = 0;
+    int _stock = 0;
+    int _debt = 0;
+    int _experience = 0;
+};
