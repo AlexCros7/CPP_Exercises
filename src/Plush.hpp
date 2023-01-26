0ffe7cc7e32f06fb8aa453161bd814a61835309b
Alexandre Cros <alexcros77700@gmail.com>
2023-01-26 12:41:50 +0100

    Test 01 to Test 09 passed

--- src/Plush.hpp
+++ src/Plush.hpp
@@ -1,3 +1,23 @@
-class Plush {
+class Plush 
+{
+public:
+    Plush()
+    {}
 
+    Plush(int cost) 
+        : _cost { cost }
+    {}
+
+    int get_cost() const
+    {
+        return _cost;
+    }
+
+    void set_cost(int newCost)
+    {
+        _cost = newCost;
+    }
+
+private:
+    int _cost = 10;
 };
