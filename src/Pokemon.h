833dca71a63ba127f494bfbe1559dee71e7a95a7
Alexandre Cros <alexcros77700@gmail.com>
2023-02-09 15:45:33 +0100

    test 1 and 2 passed

--- src/Pokemon.h
+++ src/Pokemon.h
@@ -4,4 +4,40 @@
 
 // A creature that is cute and can fight other ones.
 class Pokemon
-{};
+{
+public:
+    Pokemon(const std::string& name)
+        : _name { name }
+        , _id { _new_id ++ }
+    {}
+
+    Pokemon(const Pokemon& other)
+        : _name { other._name }
+        , _id { other._new_id ++ }
+    {}
+    
+    const std::string &name() const
+    {
+        return _name;
+    }
+
+    int id() const
+    {
+        return _id;
+    }
+
+    Pokemon& operator=(const Pokemon& other)
+    { 
+        if (this != &other)
+        {
+            _name = other._name;
+        }
+
+        return *this;
+    }
+
+private:
+    std::string _name;
+    int _id;
+    static inline int _new_id = 0;
+};
