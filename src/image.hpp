d8c3f0656969e4c4f8130747d4685cb67d7a5b61
Alexandre Cros <alexcros77700@gmail.com>
2023-03-29 12:28:51 +0200

    test 0101 to 0105 passed

--- src/image.hpp
+++ src/image.hpp
@@ -11,3 +11,59 @@
 #include <array>
 #include <iostream>
 
+template <typename P, size_t W, size_t H>
+struct Image
+{
+public:
+    Image()
+    {}
+
+    Image(const P& pix)
+    {
+        for (size_t j = 0; j < H; j++)
+        {
+            for (size_t i = 0; i < W; i++)
+            {
+                _pixels[j][i] = pix;
+            } 
+        }
+    }
+
+    Image(const std::function<P(const size_t i, const size_t j)>& functor)
+    {
+        for (size_t j = 0; j < H; j++)
+        {
+            for (size_t i = 0; i < W; i++)
+            {
+                _pixels[j][i] = functor(i,j);
+            } 
+        }
+    }
+
+    P& operator()(const size_t i, const size_t j)
+    {
+        return _pixels[j][i];
+    }
+
+    const P & operator()(const size_t i, const size_t j) const
+    {
+        return _pixels[j][i];
+    }
+
+private:
+    std::array<std::array<P, W>, H> _pixels;
+};
+
+template <typename A, size_t W, size_t H>
+Image<A,W,H> operator+(const Image<A,W,H>& lhs, const Image<A,W,H>& rhs)
+{
+    Image<A,W,H> res;
+    for (size_t j = 0; j < H; j++)
+    {
+        for (size_t i = 0; i < W; i++)
+        {
+            res(i,j) = lhs(i,j) + rhs(i,j);
+        } 
+    }
+    return res;
+}
